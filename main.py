a329ab5e1fb0de870aaa239888bb2a02d3749c0c
MrGg570 <143704858+MrGg570@users.noreply.github.com>
2024-10-10 22:28:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,34 +13,37 @@ enemy = Build.build('enemy', 100,5)
 
 clear = lambda: system('cls||clear')
 
-def display():
-    console.print(player.pvbar()+'\n')
-    console.print(enemy.pvbar()+'\n')
+# def display():
+#     console.print(player.pvbar()+'\n')
+#     console.print(enemy.pvbar()+'\n')
 
-while True:
-    clear()
-    assert player.pv > 0, "Joueur mort!"
-    display()
-    print("0 - Attaquer\n1 - Ne rien faire")
-    action = input(">>> ")
-    clear()
-    match action:
-        case "0":
-            display()
-            print(f"Vous attaquez {enemy.name}")
-            player.attaquer(enemy)
-            input()
-        case "1":
-            display()
-            print(f"Vous ne faites rien...")
-            input()
-        case _:
-            display()
-            print(f"Vous {action}!... Mais ça veut dire quoi?")
-            input()
-    assert enemy.pv > 0, "Enemy mort!"
-    clear()
-    display()
-    print(f"{enemy.name} vous attaque!")
-    enemy.attaquer(player)
-    input()
+# while True:
+#     clear()
+#     assert player.pv > 0, "Joueur mort!"
+#     display()
+#     print("0 - Attaquer\n1 - Ne rien faire")
+#     action = input(">>> ")
+#     clear()
+#     match action:
+#         case "0":
+#             display()
+#             print(f"Vous attaquez {enemy.name}")
+#             player.attaquer(enemy)
+#             input()
+#         case "1":
+#             display()
+#             print(f"Vous ne faites rien...")
+#             input()
+#         case _:
+#             display()
+#             print(f"Vous {action}!... Mais ça veut dire quoi?")
+#             input()
+#     assert enemy.pv > 0, "Enemy mort!"
+#     clear()
+#     display()
+#     print(f"{enemy.name} vous attaque!")
+#     enemy.attaquer(player)
+#     input()
+
+def rfrsh(menu):
+    pass
