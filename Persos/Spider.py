b07b9c798aedee2f7040837f976adc13ac8f07d0
MrGg570 <143704858+MrGg570@users.noreply.github.com>
2024-10-17 14:55:46 +0200

    Update Spider.py

--- Persos/Spider.py
+++ Persos/Spider.py
@@ -1,5 +1,8 @@
 from .Perso import Character
 
 class Spider(Character):
+    """
+    Classe utilisée pour instancier une Araignée
+    """
     def __init__(self, lvl) -> None:
         super().__init__('Araignée', pv=4*lvl, atk=3*lvl, arm=0, lvl=lvl, dodge=2)
