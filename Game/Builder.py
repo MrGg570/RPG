fc5d22d08346360ec22f2b7596493ea87c8f96ed
MrGg570 <143704858+MrGg570@users.noreply.github.com>
2024-10-17 15:00:46 +0200

    Update Builder.py

--- Game/Builder.py
+++ Game/Builder.py
@@ -1,11 +1,18 @@
 from Persos import PersoPrincipal, Goblin, Spider
 
 class Build:
+    """
+    Classe utiliser pour obtinir les monstres et le joueur
+    """
     def __init__(self) -> None:
         pass
 
     @classmethod
     def build(self, name: str, lvl: int = 1):
+    """
+    Le décorateur permet d'appeler la fonction sans instancier d'objet.
+    La fonction retourne le personnage demandé avec le niveau précisé 
+    """
         name = name.lower()
         if name == 'goblin':
             return Goblin.Goblin(lvl)
