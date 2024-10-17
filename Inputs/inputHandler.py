d6bbe385c4d203984749071d18c8c6fc418216a5
MrGg570 <143704858+MrGg570@users.noreply.github.com>
2024-10-17 14:32:31 +0200

    Update inputHandler.py

--- Inputs/inputHandler.py
+++ Inputs/inputHandler.py
@@ -9,9 +9,13 @@ class Input:
         self.console = console
 
     def get_keyboard_input(self) -> str:
+        """
+        Permet d'attendre jusqu'à ce qu'une touche sois pressé parmis celles utilisées par le jeu (ici flèche du haut, flèche du bas ou entrer)
+        """
         while is_pressed('haut') or is_pressed('bas') or is_pressed('enter'):
             sleep(0.05)
         input = False
+        """Cette variable représente la dernière touche appuyée"""
         while not input:
             if is_pressed('haut'):
                 input = 'haut'
