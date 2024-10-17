f0003156e70fe6531dd842a12865031380b93c89
MrGg570 <143704858+MrGg570@users.noreply.github.com>
2024-10-17 14:59:13 +0200

    Update Forest.py

--- Regions/Forest.py
+++ Regions/Forest.py
@@ -1,5 +1,8 @@
 from .Region import Region
 
 class Forest(Region):
+    """
+    Classe utilisée pour instancier une région de type Forêt
+    """
     def __init__(self, minlvl, maxlvl) -> None:
         super().__init__("Fôret Profonde", minlvl, maxlvl, ['goblin','spider'], maxmob=3)
